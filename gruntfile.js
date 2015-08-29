b5f611745d4077e1e3f763b7853251983f5c2236
manavm1990 <manavm@visionwebsoft.com>
2015-08-29 09:52:48 -0500

    second commit

--- gruntfile.js
+++ gruntfile.js
@@ -5,41 +5,53 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		bootlint: {
 			options: {
+				relaxerror: ['W003'],
 				showallerrors: true
 			},
 			files: ['src/*.html']
 		},
 		csslint: {
-			options: {
-				quiet: true
-			},
 			src: ['src/css/tcs.css']
 		},
 		cssmin: {
 			target: {
-					files: {
-						'css/styles.min.css': ['src/css/*.css']
-					}
+				files: {
+					'css/styles.min.css': ['src/css/*.css']
+				}
+			}
+		},
+		htmlmin: { // Task 
+			dist: { // Target 
+				options: { // Target options 
+					removeComments: true,
+					collapseWhitespace: true,
+					minifyJS: true
+				},
+				files: { // Dictionary of files 
+					'index.html': 'src/index.html', // 'destination': 'source' 
 				}
 			},
-		htmltidy: {
-			compileMultiple: {
-				files: [{
-					expand: true,
-					cwd: 'src',
-					src: ['index.html'],
-					dest: ''
-				}],
-				options: {
-					doctype: 'html5',
-					hideComments: true,
-					indent: true
+			dev: { // Another target 
+				// options: {
+				// 	lint: true
+				// },
+				files: {
+					'index.html': 'src/index.html'
 				}
+			}
+		},
+		sprite: {
+			services: {
+				src: ['src/img/flex*.jpg', 'src/img/mag*.jpg', 'src/img/rig*.jpg', 'src/img/stencils*.jpg'], // filter out the @2x images
+				retinaSrcFilter: ['src/img/*@2x.jpg'],
+				dest: 'img/icons/spritesheet.services.jpg',
+				retinaDest: 'img/icons/spritesheet.services@2x.jpg',
+				destCss: 'css/sprite.css'
 			}
 		},
 		jshint: {
 			// define the file(s) to lint
-			files: ['src/js/tcs.js'],
+			files: ['src/js/*.js'],
 			// configure JSHint (documented at http://www.jshint.com/docs/)
 			options: {
 				// more options here if you want to override JSHint defaults
@@ -63,13 +75,13 @@ module.exports = function(grunt) {
 
 				// Developer's Note: These key-values are inside the dev object!
 				src: 'src/js/*.js',
-				dest: 'js/script.min.js' 
+				dest: 'js/script.min.js'
 			}
 		},
-		watch:{
+		watch: {
 			html: {
 				files: ['src/*.html'],
-				tasks: ['bootlint', 'htmltidy'],
+				tasks: ['bootlint', 'htmlmin'],
 				options: {
 					// Start a live reload server on the default port 35729
 					livereload: true
@@ -84,7 +96,7 @@ module.exports = function(grunt) {
 				}
 			},
 			js: {
-				files: ['src/js/*.js'],
+				files: ['src/js/*.js', 'gruntfile.js'],
 				tasks: ['jshint', 'uglify:dev']
 			}
 		}
@@ -92,15 +104,16 @@ module.exports = function(grunt) {
 
 	// Load the plugins
 	grunt.loadNpmTasks('grunt-bootlint');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 	grunt.loadNpmTasks('grunt-contrib-csslint');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-htmltidy');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-spritesmith');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	
-	// Register task(s)
-	grunt.registerTask('default', ['bootlint', 'cssmin', 'csslint', 'htmltidy', 'jshint', 'uglify:dev']);
-	grunt.registerTask('dist', ['uglify:dist']);
 
+	// Register task(s)
+	grunt.registerTask('default', ['bootlint', 'htmlmin:dev', 'csslint', 'cssmin', 'jshint','uglify:dev']);
+	grunt.registerTask('dist', ['htmlmin:dist', 'uglify:dist']);
+	grunt.registerTask('spriteGen', ['sprite'])
 };
