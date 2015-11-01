60d008e99c6d0136b8132b37d97be652dcc27c17
manavm1990 <manavm@visionwebsoft.com>
2015-10-31 22:31:41 -0500

    Fix: 100% height...
    Had to resort to using JS to manipulate the CSS!

---
+++ src/js/height.js
@@ -0,0 +1,20 @@
+/*
+	* Problem: Setting height of CSS is just not achieving consistent '100% height'
+	*  results, without specifying a px height for <html>.
+	*  Solution: Get the height of the window upon page load and change the CSS of <html
+	*  'height' property to match. 
+ */
+
+$(document).ready(function() {
+	/*
+		* Designer's Note: Scrollheight is a property - not a method, so no '().'
+		*  ScrollHeight is really what gets the 'entire' page height - the whole DOM!
+		*  (https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollHeight)
+	 */
+	var html = document.getElementsByTagName("html")[0]; // Need "" around tag name.
+	var docHeight = html.scrollHeight + "px"; // Need units on this numerical value.
+
+	// Use .style.property to change the height property of <html>. 
+	html.style.height = docHeight;
+	console.log(html.style.height);
+});
