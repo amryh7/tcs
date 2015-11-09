0bab1357ded4479ab911a6d36188ed0d8a265994
amryh7 <amryh7@gmail.com>
2015-11-08 18:44:00 -0600

    Feature: Banner img animation

---
+++ src/js/img-anm.js
@@ -0,0 +1,94 @@
+function AnimateBanner(){
+	//alert("linked");	
+	$('#band-img01').on('click',function(){});
+	$('#band-img01').trigger('click');
+	var aptx = 
+	'<!-- Save for Web Slices (brand.gif) -->' +
+			'<table id="Table_01" width="513" height="85" border="0" cellpadding="0" cellspacing="0">' +
+				'<tr>' +
+					'<td>' +
+						'<img id="band-img01" class="band-table-img" srcset="img/banner/brand_01.gif 1x, img/banner/brand@2x_01 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+					'<td>' +
+						'<img id="band-img02" class="band-table-img" srcset="img/banner/brand_02.gif 1x, img/banner/brand@2x_02 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+					'<td>' +
+						'<img id="band-img03" class="band-table-img" srcset="img/banner/brand_03.gif 1x, img/banner/brand@2x_03 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+					'<td>' +
+						'<img id="band-img04" class="band-table-img" srcset="img/banner/brand_04.gif 1x, img/banner/brand@2x_04 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+					'<td>' +
+						'<img id="band-img05" class="band-table-img" srcset="img/banner/brand_05.gif 1x, img/banner/brand@2x_05 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+					'<td>' +
+						'<img id="band-img06" class="band-table-img" srcset="img/banner/brand_06.gif 1x, img/banner/brand@2x_06 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+					'<td>' +
+						'<img id="band-img07" class="band-table-img" srcset="img/banner/brand_07.gif 1x, img/banner/brand@2x_07 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+					'<td>' +
+						'<img id="band-img08" class="band-table-img" srcset="img/banner/brand_08.gif 1x, img/banner/brand@2x_08 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+					'<td>' +
+						'<img id="band-img09" class="band-table-img" srcset="img/banner/brand_09.gif 1x, img/banner/brand@2x_09 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+					'<td>' +
+						'<img id="band-img10" class="band-table-img" srcset="img/banner/brand_10.gif 1x, img/banner/brand@2x_10 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+				'</tr>' +
+				'<tr>' +
+					'<td>' +
+						'<img id="band-img11" class="band-table-img" srcset="img/banner/brand_11.gif 1x, img/banner/brand@2x_11 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+					'<td>' +
+						'<img id="band-img12" class="band-table-img" srcset="img/banner/brand_12.gif 1x, img/banner/brand@2x_12 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+					'<td>' +
+						'<img id="band-img13" class="band-table-img" srcset="img/banner/brand_13.gif 1x, img/banner/brand@2x_13 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+					'<td>' +
+						'<img id="band-img14" class="band-table-img" srcset="img/banner/brand_14.gif 1x, img/banner/brand@2x_14 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+					'<td>' +
+						'<img id="band-img15" class="band-table-img" srcset="img/banner/brand_15.gif 1x, img/banner/brand@2x_15 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+					'<td>' +
+						'<img id="band-img16" class="band-table-img" srcset="img/banner/brand_16.gif 1x, img/banner/brand@2x_16 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+					'<td>' +
+						'<img id="band-img17" class="band-table-img" srcset="img/banner/brand_17.gif 1x, img/banner/brand@2x_17 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+					'<td>' +
+						'<img id="band-img18" class="band-table-img" srcset="img/banner/brand_18.gif 1x, img/banner/brand@2x_18 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+					'<td>' +
+						'<img id="band-img19" class="band-table-img" srcset="img/banner/brand_19.gif 1x, img/banner/brand@2x_19 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+					'<td>' +
+						'<img id="band-img20" class="band-table-img" srcset="img/banner/brand_20.gif 1x, img/banner/brand@2x_20 2x" width="52" height="43" alt="the component store logo in Eyehawk font, skewed, red lettering, with black brush script underneath reading: '+"'Simply Resourceful'"+'"></td>' +
+				'</tr>' +
+			'</table>' +
+		'<!-- End Save for Web Slices -->';
+		
+	$('#band-img').append(aptx);
+	$('.band-table-img').each(function(){
+		$(this).css({"margin":"0px"});
+		});
+
+	band01 = $('#band-img01');
+	band02 = $('#band-img02');
+	band03 = $('#band-img03');
+	band04 = $('#band-img04');
+	band05 = $('#band-img05');
+	band06 = $('#band-img06');
+	band07 = $('#band-img07');
+	band08 = $('#band-img08');
+	band09 = $('#band-img09');
+	band10 = $('#band-img10');
+	band11 = $('#band-img11');
+	band12 = $('#band-img12');
+	band13 = $('#band-img13');
+	band14 = $('#band-img14');
+	band15 = $('#band-img15');
+	band16 = $('#band-img16');
+	band17 = $('#band-img17');
+	band18 = $('#band-img18');
+	band19 = $('#band-img19');
+	band20 = $('#band-img20');
+	var banArray = [band01, band02, band03, band04, band05, band06, band07, band08, band09, band10, band11, band12, band13, band14, band15, band16, band17, band18, band19, band20];
+	
+	setTimeout(function(){
+		for(i = 0; i < banArray.length; i++){
+			banArray[i].css({
+							"transition":"all 3s linear",
+							"background":"rgb(154, 156, 157) url('assets/imgs/ex-2.png') no-repeat center center",
+							"background-size":"60%",
+							"-ms-transform":"rotate(720deg)", /* IE 9 */
+							"-webkit-transform":"rotate(720deg)", /* Chrome, Safari, Opera */
+							"transform":"rotate(720deg)"
+			});
+		};
+	},3500);
+};
